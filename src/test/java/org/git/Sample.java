d3e127e628abc7bf7329dd4ba34c5d3a417feed7
Shanmugam2703 <shanmugamr273@gmail.com>
2021-01-27 09:34:47 +0530

    Updated code

--- src/test/java/org/git/Sample.java
+++ src/test/java/org/git/Sample.java
@@ -3,7 +3,7 @@ package org.git;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Test A1");
-		System.out.println("Test A3");
+		System.out.println("Test A5");
 	}
 
 }
